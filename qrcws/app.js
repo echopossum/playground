69405d7bd89edaf39a069ff1309958a63f73af8c
kevin <kevin@forwardthinkingdesigns.com>
2025-06-12 16:38:09 -0400

    Initial Q-Sys connection

--- qrcws/app.js
+++ qrcws/app.js
@@ -1,1 +1,17 @@
-console.log(process.env.CORE_IP, process.env.PORT)
+import { Qrwc } from '@q-sys/qrwc'
+
+const socket = new WebSocket(`ws://${process.env.CORE_IP}/qrc-public-api/v0`)
+
+
+
+const components = await Qrwc.createQrwc({
+  socket,
+  pollingInterval: 350 // Optional: polling interval in milliseconds (default: 350)
+})
+
+
+const gain = components.components.Gain01.controls.gain
+gain.on('update', (state) =>{
+    console.log(state)
+})
+
