fa2766e6f8cac9e7cb45abfba02368bfb5a7f0b3
kevin <kevin@forwardthinkingdesigns.com>
2025-06-13 13:04:12 -0400

    Implement express server

--- qrcws/app.js
+++ qrcws/app.js
@@ -1,22 +1,23 @@
 import { Qrwc } from '@q-sys/qrwc'
+import express from 'express'
+
+const app = express()
+
+
+app.get('/',(req,res) =>{
+  res.send('Hello World')
+})
 
 const socket = new WebSocket(`ws://${process.env.CORE_IP}/qrc-public-api/v0`)
 
-const compys = {}
+
 
 const qsys = await Qrwc.createQrwc({
   socket,
   pollingInterval: 350 // Optional: polling interval in milliseconds (default: 350)
 })
 
-const gain = qsys.components.Gain01.controls.gain
-gain.on('update', (state) =>{
-    //state event handler here
+
+app.listen(process.env.PORT,()=>{
+  console.log(`Server listenting on port:${process.env.PORT}`)
 })
-
-
-console.table(qsys.components)
-console.log(typeof(qsys.components))
-qsys.components.forEach(element => {
-    console.log(element)
-});
