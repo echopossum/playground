55d681a3d56483cbeec6b62be2899500f0b6b0d9
kevin <kevin@forwardthinkingdesigns.com>
2025-06-12 21:30:59 -0400

    Pre home

--- qrcws/app.js
+++ qrcws/app.js
@@ -2,17 +2,21 @@ import { Qrwc } from '@q-sys/qrwc'
 
 const socket = new WebSocket(`ws://${process.env.CORE_IP}/qrc-public-api/v0`)
 
+const compys = {}
 
-
-const components = await Qrwc.createQrwc({
+const qsys = await Qrwc.createQrwc({
   socket,
   pollingInterval: 350 // Optional: polling interval in milliseconds (default: 350)
 })
 
-
-const gain = components.components.Gain01.controls.gain
+const gain = qsys.components.Gain01.controls.gain
 gain.on('update', (state) =>{
-    console.log(state)
+    //state event handler here
 })
 
-gain.update(-100)
+
+console.table(qsys.components)
+console.log(typeof(qsys.components))
+qsys.components.forEach(element => {
+    console.log(element)
+});
